61887b7dc659dcdad379ac39f793059676f552d1
Ahmed-Abdelkhalek <41132805+ahmedabdelkhalek1@users.noreply.github.com>
2024-12-06 13:52:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// Smooth scroll for navigation
 document.querySelectorAll('nav a').forEach(link => {
-  link.addEventListener('click', e => {
-    e.preventDefault();
-    const sectionId = link.getAttribute('href').substring(1);
-    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+  link.addEventListener('click', event => {
+    event.preventDefault();
+    const targetId = link.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    targetElement.scrollIntoView({ behavior: 'smooth' });
   });
 });
